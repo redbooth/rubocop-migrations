b7e288c634ab74578b4837c558924a2fa541b788
Jose Fernandez (magec) <joseferper@gmail.com>
2016-09-22 16:15:28 +0200

    better specs

--- spec/rubocop/cop/migrations/remove_index_spec.rb
+++ spec/rubocop/cop/migrations/remove_index_spec.rb
@@ -1,3 +1,4 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'spec_helper'))
 describe RuboCop::Cop::Migrations::RemoveIndex do
   include SharedExamples
   subject(:cop) { described_class.new }
